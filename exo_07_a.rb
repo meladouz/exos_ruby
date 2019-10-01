78294640858e4f88d10d8eb6ec9bbbd6815757e3
Jordi-LG <jordy.leguet@gmail.com>
2019-10-01 11:12:10 +0200

    add exo7a and exo7b

---
+++ exo_07_a.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+#gets indiquera /n à la fin de la valeur de la variable user_name, correspondant à un retour à la ligne
+#gets.chomp renvoie sans le caractère /n, avec un retour quand même
+# Valable seulement en interactive Ruby (irb), pas en script.rb
